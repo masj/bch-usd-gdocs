ecdda8ad6f162c4aa87e48e24d769b76b82771f1
MasJ <masj@emuparadise.org>
2017-08-31 11:48:04 +0100

    adding readme and ticker file - ready to use now

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# bch-usd-gdocs
+This is a ticker macro for google docs.
+
+If you want to use live bitcoin cash->USD exchange rates in your google document, you can use this macro. Currently written for USD rates but can easily be modified for Euro or any other currency.
+
+It uses the coinmarketcap API. It will cache the rate to avoid hitting the servers repeatedly.
+
+Usage instructions can be found in ticker.js
---
+++ ticker.js
@@ -0,0 +1,82 @@
+
+// A macro for google docs that will help you get BTC exchange rates into your excel sheets
+// How to Use:
+// 1. Create a new google spreadsheet
+// 2. Go to Tools -> Script Editor
+// 3. Delete everything in the edit window and replace with the code of this file
+// 4. Click on the Save button (floppy disk icon)
+// 5. Go back to your document and fetch the rate using: =fetchTicker("coinmarketcap")
+// 6. You can replace "bitfloor" with "mtgox" or "bitpay" for the rate you want (you can also get all 3 rates in the same document)
+
+
+function getBalance(btcAddress) {
+    var response = UrlFetchApp.fetch('http://blockchain.info/address/' + btcAddress + '?format=json');
+    var json = response.getContentText();
+    var data = JSON.parse(json);
+    return data.final_balance * Math.pow(10,-8);
+}
+
+function donothammer()
+{
+    var cache = CacheService.getPublicCache()
+    var now = (new Date()).getTime()
+    var last = cache.get('last')
+
+    if (null != last) {
+        var lastTime = parseInt(last)
+        var elapsed = now - lastTime
+        var waitFor = 1 - elapsed
+        if (0<waitFor)
+            Utilities.sleep(waitFor)
+    }
+    
+    cache.put('last', String(now))
+}
+
+function fetchCached(url)
+{
+    var publicCache = CacheService.getPublicCache()
+    var cached = publicCache.get(url)
+
+    if (null == cached) {
+    
+        donothammer()
+        
+        var response = UrlFetchApp.fetch(url)
+        if ('undefined' != typeof(response)) {
+            var code = response.getResponseCode()
+            if (code<300) {
+                var oneMin = 1 * 60
+                cached = response.getContentText()
+                publicCache.put(url, cached, oneMin)
+            }
+        }
+    }
+
+    return cached
+}
+
+function fetchTicker(
+    tickerName     // e.g. :  'coinmarketcap'
+)
+{
+    var url = "https://api.coinmarketcap.com/v1/ticker/bitcoin-cash/"
+
+    var r = fetchCached(url)
+    if ('undefined' == typeof(r))
+        return 'No data for ticker ' + tickerName
+        
+    r = Utilities.jsonParse(r)
+    if ('undefined' == typeof(r))
+        return 'Malformed JSON data returned by ticker ' + tickerName
+    
+    if (tickerName == 'coinmarketcap')
+        r = r[0]['price_usd']
+
+    if ('undefined' == typeof(r))
+        return 'Unknown field ' + fieldName + ' for ticker ' + tickerName
+
+    return r
+}
+
+
